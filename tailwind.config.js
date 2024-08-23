00395e05a3b62173849b64d509868638d8235a74
abdurFaiz <115395387+abdurFaiz@users.noreply.github.com>
2024-08-23 22:39:01 +0700

    new

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      colors: {
+        primary: "#FF9357",
+        secondary: "#323437",
+        text_paragraph : "#a8a8ab",
+        gradient_card : "#050211",
+        bg_dark_card: "#202020",
+        input_placeholder: "#7D7B91",
+        grey: "#353535",
+        dark: "#141414	",
+      },
+      fontFamily: {
+        Poppins: ["Poppins"],
+      },
+    },
+  },
+  plugins: [],
+};
