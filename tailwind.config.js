63c852e7c9c68eb46a49f057cfb731d61886f114
abdurFaiz <115395387+abdurFaiz@users.noreply.github.com>
2024-08-24 17:04:57 +0700

    move index.html to root

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
     extend: {
       container: {
@@ -9,8 +9,8 @@ module.exports = {
       colors: {
         primary: "#FF9357",
         secondary: "#323437",
-        text_paragraph : "#a8a8ab",
-        gradient_card : "#050211",
+        text_paragraph: "#a8a8ab",
+        gradient_card: "#050211",
         bg_dark_card: "#202020",
         input_placeholder: "#7D7B91",
         grey: "#353535",
